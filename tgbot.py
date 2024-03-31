ad3ff6bc0bcd452a480af421819bc751d2091c98
Валерия Лапкина <vlrlapkina@gmail.com>
2024-03-31 13:00:32 +0600

    bot

--- tgbot.py
+++ tgbot.py
@@ -4,9 +4,10 @@ import os
 from dotenv import load_dotenv
 from telethon import TelegramClient, events
 
-
+# Load environment variables from a .env file
 load_dotenv()
 
+# Initialize Telegram client with API credentials and start session
 api_id = os.getenv('API_ID')
 api_hash = os.getenv('API_HASH')
 bot_token = os.getenv('TOKEN')
@@ -16,44 +17,36 @@ client = TelegramClient('session_name',
 
 
 def format_timestamp(timestamp):
-    return datetime.datetime.fromtimestamp(timestamp,
-                                           datetime.timezone.utc).astimezone(
-                                           ).strftime('%Y-%m-%d %H:%M:%S %Z')
+    return datetime.datetime.fromtimestamp(
+        timestamp,
+        datetime.timezone.utc).astimezone().strftime('%Y-%m-%d %H:%M:%S %Z')
 
 
 @client.on(events.NewMessage(pattern='/start'))
 async def start(event):
-    try:
-        name = event.sender.first_name
-        await event.respond('Привет {}, введи @никнейм пользователя, '
-                            'чтобы получить информацию'.format(name))
-
-    except Exception:
-        pass
+    name = event.sender.first_name
+    await event.respond('Привет {}, введи @никнейм пользователя, '
+                        'чтобы получить информацию:)'.format(name))
 
 
 @client.on(events.NewMessage)
 async def get_user_info(event):
     username = event.raw_text.strip()
-    try:
-        user = await client.get_entity(username)
-        user_info = f"Имя пользователя: {user.username}\n"
-        user_info += f"Имя: {user.first_name}\n"
-        user_info += f"Фамилия: {user.last_name}\n"
-        user_info += f"ID пользователя: {user.id}\n"
+    user = await client.get_entity(username)
+    user_info = f"Имя пользователя: {user.username}\n"
+    user_info += f"Имя: {user.first_name}\n"
+    user_info += f"Фамилия: {user.last_name}\n"
+    user_info += f"ID пользователя: {user.id}\n"
+    user_info += f"Номер телефона: {user.phone}\n"
 
-        # Сохранение фотографии аватарки, если она есть
-        photos = await client.get_profile_photos(user.id)
+    # Download profile photos and send them
+    photos = await client.get_profile_photos(user.id)
+    for i, photo in enumerate(photos):
+        photo_file = await client.download_media(photo)
+        photo_path = f"{username}_profile_photo_{i}.jpg"
+        os.rename(photo_file, photo_path)
+        await event.respond(file=photo_path)
 
-        # Сохранение фотографий и отправка их
-        for i, photo in enumerate(photos):
-            photo_file = await client.download_media(photo)
-            photo_path = f"{username}_profile_photo_{i}.jpg"
-            os.rename(photo_file, photo_path)
-            await event.respond(file=photo_path)
-
-        user_info += f"Количество фотографий профиля: {len(photos)}"
-        await event.respond(user_info)
-    except Exception:
-        pass
+    user_info += f"Количество фотографий профиля: {len(photos)}"
+    await event.respond(user_info)
 client.run_until_disconnected()
